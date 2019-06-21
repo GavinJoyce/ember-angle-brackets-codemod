a3389d39ae79bd3f56963f9e5a7443211aa7ea15
Gavin Joyce <gavinjoyce@gmail.com>
2019-06-21 10:04:37 +0100

    fixup!

--- transforms/angle-brackets/transforms/angle-brackets-syntax.js
+++ transforms/angle-brackets/transforms/angle-brackets-syntax.js
@@ -469,7 +469,7 @@ module.exports = function(fileInfo, api, options) {
   // Haxx out valueless data-* and args with the empty string 
 
   // console.log('AST', JSON.stringify(ast));
-  let uglySource = glimmer.print(astJson).replace(attrEqualEmptyString,"");
+  let uglySource = glimmer.print(ast).replace(attrEqualEmptyString,"");
   // console.log('GJ: output from codemod', uglySource);
   let dataOk = uglySource.replace(dataEqualsNoValue, "$1");
 
